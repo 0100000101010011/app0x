754c15a678c3995e456edbe3b7588c05937294b3
0100000101010011 <sanchez.alvin@gmail.com>
2021-08-22 17:52:57 -0500

    refactor(css/js): adds sticky nav fix

--- public/views/assets/js/app0.js
+++ public/views/assets/js/app0.js
@@ -40,3 +40,39 @@ console.log(isMobileDevice());
 if (isMobileDevice() === true) {
   viewSizes.classList.toggle('hide');
 }
+
+// Offset sticky nav
+
+// res https://stackoverflow.com/questions/19158559/how-to-fix-a-header-on-scroll
+function findOffset(element) {
+  var top = 0,
+    left = 0;
+
+  do {
+    top += element.offsetTop || 0;
+    left += element.offsetLeft || 0;
+    element = element.offsetParent;
+  } while (element);
+
+  return {
+    top: top,
+    left: left,
+  };
+}
+
+window.onload = function () {
+  var stickyHeader = document.getElementById('sticky');
+  var headerOffset = findOffset(stickyHeader);
+
+  window.onscroll = function () {
+    // body.scrollTop is deprecated and no longer available on Firefox
+    var bodyScrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (bodyScrollTop > headerOffset.top) {
+      stickyHeader.classList.add('fixed');
+    } else {
+      stickyHeader.classList.remove('fixed');
+    }
+  };
+};
